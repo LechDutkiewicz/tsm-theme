65861fc0a9224a4e3c3ce00f7a3844a93faea511
LechDutkiewicz <lechdutkiewicz@gmail.com>
2018-04-18 11:30:51 +0200

    rozszerzenie z mobilnym kolorem paska nawigacji

---
+++ lib/extensions/mobile_address_bar/mobile_address_bar.php
@@ -0,0 +1,17 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) )
+	exit( 'No direct script access allowed' ); // Exit if accessed directly
+
+function address_mobile_address_bar() {
+	$color = "#84bcad";
+	//this is for Chrome, Firefox OS, Opera and Vivaldi
+	echo '<meta name="theme-color" content="'.$color.'">';
+	//Windows Phone **
+	echo '<meta name="msapplication-navbutton-color" content="'.$color.'">';
+	// iOS Safari
+	echo '<meta name="apple-mobile-web-app-capable" content="yes">';
+	echo '<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">';
+}
+add_action( 'wp_head', 'address_mobile_address_bar' );
+?>
